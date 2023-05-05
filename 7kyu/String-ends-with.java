4dec6d58e748a6c818c8ece2b05fcd8a0eaaae5b
Gabriel Vallejo <gabi.vallejo44@gmail.com>
2023-05-05 15:56:56 +0200

    nuevo codewars challenge

---
+++ 7kyu/String-ends-with.java
@@ -0,0 +1,15 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//Solución
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    int lengthStart = str.length()-ending.length();
+    if( lengthStart >= 0 && str.substring(lengthStart).equals(ending) ) {
+      return true;
+    }
+    return false;    
+  }
+}
